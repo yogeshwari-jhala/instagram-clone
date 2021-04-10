cecc70eb97b929b2eda8c9952d28afacbf7b8d34
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-11 01:06:17 +0530

    Insta clone uploaded

---
+++ src/Post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import Suresh from './reactlogo.jpg'
+import './Post.css'
+import Avatar from "@material-ui/core/Avatar"
+// import Ramesh from './insta.png'
+function Post({username, caption , imageUrl})
+{
+    return (
+        <div className="post">
+            <div className="post__header">
+            <Avatar 
+            className="post__avatar"
+            alt='Yogeshwari'
+            src="/static/images/avatar/1.jpg" />
+            <h3>{username}</h3>
+            </div>
+            <img className="post__image" src={imageUrl}/>
+            <h4 className="post__text"><strong>{username}</strong>{caption}</h4>
+            
+        </div>
+    )
+}
+
+export default Post
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+const firebaseApp =firebase.initializeApp({
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyCewKJvG_M3L6Flu_Uj_V_NkMheYg0n46s",
+  authDomain: "instagram-ed9fa.firebaseapp.com",
+  projectId: "instagram-ed9fa",
+  storageBucket: "instagram-ed9fa.appspot.com",
+  messagingSenderId: "888246055365",
+  appId: "1:888246055365:web:b520209176abcfd1591bac",
+  measurementId: "G-FRDRZHRHXS"
+  });
+
+  
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const storage = firebase.storage();
+
+  export {db, auth, storage};
+
