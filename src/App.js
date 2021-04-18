845327add055cd3e0bcf22347f993ad917950672
yogeshwari27 <yogeshwarijhala1627@gmail.com>
2021-04-19 00:22:09 +0530

    comment is added

--- src/App.js
+++ src/App.js
@@ -201,7 +201,7 @@ const signIn = (event) => {
         <h1>Hello welcome to my instagram</h1>
         {
           posts.map(({id, post} ) =>(
-            <Post key={id}  username={post.username}  caption={post.caption} imageUrl={post.imageUrl} />
+            <Post key={id} postId={id} username={post.username}  caption={post.caption} imageUrl={post.imageUrl} />
           ))
         }
     </div>
