c5d2f650e7b598b4aa2b062f3109c3add232a228
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-19 18:20:59 +0530

    Sign Page

---
+++ src/View/Sign/Sign.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Sign extends React.Component{
+    constructor (props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+}
+
+export default Sign
