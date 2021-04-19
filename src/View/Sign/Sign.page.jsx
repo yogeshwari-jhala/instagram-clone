ee38094b59801226db3d8fbe34e7ce87b7df0266
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-19 23:50:35 +0530

    updates

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
+import LoginImage from "../../OldComponent/assets/instalogo.png";
+import './Login.style.scss'
 class SignUpPage extends React.Component{
     render(){
         return(
-            <div/>
+            <div className="base_container">
+                <div className="header">Login</div>
+                <div className="content">
+                    <div className="image">
+                        <img src={LoginImage} alt="instagramImage"/>
+                    </div>
+                    <div className="form">
+                        <div className="form_group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username"/>
+                        </div>
+                        <div className="form_group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="password"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button type="button" className="btn">
+                        Login
+                    </button>
+                </div>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Sign from './View/Sign/Page.page'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sign />
   </React.StrictMode>,
   document.getElementById('root')
 );
