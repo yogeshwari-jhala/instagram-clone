e3431603bc53796ba1e5c2f9535c0c03765dcb5f
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-20 22:35:09 +0530

    page now working

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -1,56 +1,113 @@
-import React from 'react'
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+// import Instagram from '../../assets/instalogo.png'
 
-import LoginImage from "../../assets/instalogo.png";
-import './Login.style.scss'
-class SignUpPage extends React.Component{
-    render(){
-        return(
-            <div className="base_container">
-                <div className="header">Login</div>
-                <div className="content">
-                    <div className="image">
-                        <img src={LoginImage} alt="instagramImage"/>
-                    </div>
-                    <div className="form">
-                        <div className="form_group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" name="username" placeholder="username"/>
-                        </div>
-                        <div className="form_group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="password"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button type="button" className="btn">
-                        Login
-                    </button>
-                </div>
-            </div>
-        )
-    }
+import "./Login.style.scss";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const SignInPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}></Box>
+    </Container>
+  );
+};
+
+class SignUpPage extends React.Component {
+  render() {
+    return <div />;
+  }
 }
 
-class SignInPage extends React.Component{
-    render(){
-        return(
-            <div/>
-        )
-    }
-}
-
-export {SignInPage, SignUpPage}
+export default SignInPage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Sign from './View/Sign/Page.page'
+import Page from './View/Sign/Page.page'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Sign />
+    <Page />
   </React.StrictMode>,
   document.getElementById('root')
 );
