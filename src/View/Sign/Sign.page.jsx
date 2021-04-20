7bf8671d7ebe28798c3451911e19c3be85d9b0d5
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-20 23:38:57 +0530

    changes

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -1,51 +1,17 @@
-import React from 'react'
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-<<<<<<< HEAD
-import LoginImage from "../../assets/instalogo.png";
-import './Login.style.scss'
-class SignUpPage extends React.Component{
-    render(){
-        return(
-            <div className="base_container">
-                <div className="header">Login</div>
-                <div className="content">
-                    <div className="image">
-                        <img src={LoginImage} alt="instagramImage"/>
-                    </div>
-                    <div className="form">
-                        <div className="form_group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" name="username" placeholder="username"/>
-                        </div>
-                        <div className="form_group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder="password"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="footer">
-                    <button type="button" className="btn">
-                        Login
-                    </button>
-                </div>
-            </div>
-        )
-    }
-}
-=======
 import "./Login.style.scss";
 
 const useStyles = makeStyles((theme) => ({
@@ -131,43 +97,8 @@ const SignInPage = () => {
           </Grid>
         </form>
       </div>
-      </Container>
+    </Container>
   );
 };
->>>>>>> 03b69f39651cb8de23add0848bb727ca5cfb05ff
 
-class SignInPage extends React.Component{
-    render(){
-        return(
-            <div className="base_container">
-            <div className="header">Register</div>
-            <div className="content">
-                <div className="image">
-                    <img src={LoginImage} alt="instagramImage"/>
-                </div>
-                <div className="form">
-                    <div className="form_group">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" placeholder="username"/>
-                    </div>
-                    <div className="form_group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="email"/>
-                    </div>
-                    <div className="form_group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="password"/>
-                    </div>
-                </div>
-            </div>
-            <div className="footer">
-                <button type="button" className="btn">
-                   Register
-                </button>
-            </div>
-            </div>
-        )
-    }
-}
-
-export {SignInPage, SignUpPage}
+export default SignInPage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Sign from './View/Sign/Page.page'
-=======
-import Page from './View/Sign/Page.page'
->>>>>>> 03b69f39651cb8de23add0848bb727ca5cfb05ff
 
 
 ReactDOM.render(
