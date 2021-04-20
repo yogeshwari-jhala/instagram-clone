80a2c2bf41132b7d9f92613218484313ae0c3734
yogeshwari27 <yogeshwarijhala1627@gmail.com>
2021-04-20 22:30:28 +0530

    Sign In and Sign Up Page

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -48,7 +48,33 @@ class SignUpPage extends React.Component{
 class SignInPage extends React.Component{
     render(){
         return(
-            <div/>
+            <div className="base_container">
+            <div className="header">Register</div>
+            <div className="content">
+                <div className="image">
+                    <img src={LoginImage} alt="instagramImage"/>
+                </div>
+                <div className="form">
+                    <div className="form_group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" placeholder="username"/>
+                    </div>
+                    <div className="form_group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" placeholder="email"/>
+                    </div>
+                    <div className="form_group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder="password"/>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <button type="button" className="btn">
+                   Register
+                </button>
+            </div>
+            </div>
         )
     }
 }
