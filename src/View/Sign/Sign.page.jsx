8e0b2a698ea214f424d0475ea9b45b65989013a4
yogeshwari27 <yogeshwarijhala1627@gmail.com>
2021-04-21 13:35:34 +0530

    button added

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -74,7 +74,15 @@ const SignInPage = () => {
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
           />
-          
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
           <Grid container>
             <Grid item xs>
               <Link href="#" variant="body2">
