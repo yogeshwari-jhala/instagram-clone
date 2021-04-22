c9524aa967031f8b30aa4410126de989dbb91d66
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-22 21:52:25 +0530

    git push

--- src/View/Sign/Sign.page.jsx
+++ src/View/Sign/Sign.page.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./Login.style.scss";
-import logo from '../../assets/instalogo.png'
+import logo from "../../assets/instalogo.png";
 
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Button from 'react-bootstrap/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   margin: {
     margin: theme.spacing(1),
@@ -25,15 +26,16 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
   },
   textField: {
-    width: '25ch',
+    width: "25ch",
   },
 }));
 
-export const Login = props => {
-  const {containerRef} = props
+export const Login = (props) => {
+  const { containerRef } = props;
   const classes = useStyles();
   const [values, setValues] = React.useState({
-    password: '',
+    username: "",
+    password: "",
     showPassword: false,
   });
 
@@ -49,41 +51,78 @@ export const Login = props => {
     event.preventDefault();
   };
 
-  return(
+  return (
     <div className="base-container" ref={containerRef}>
       <div className="content">
-          <div className="image">
-            <img src={logo} alt="" />
+        <div className="image">
+          <img src={logo} alt="" />
+        </div>
+        
+          <div className="my-1">
+            <FormControl
+              className={
+                (clsx(classes.margin, classes.textField),
+                "makeStyles-textField-16")
+              }
+              variant="outlined"
+              size="small"
+            >
+              <InputLabel htmlFor="username">Email</InputLabel>
+              <OutlinedInput
+                id="username"
+                type="email"
+                value={values.username}
+                onChange={handleChange("username")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <AccountCircleIcon />
+                  </InputAdornment>
+                }
+                labelWidth={50}
+              />
+            </FormControl>
           </div>
-          <div className="form">
-          <FormControl className={clsx(classes.margin, classes.textField)} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={70}
-          />
-        </FormControl>
+          <div className="my-1">
+            <FormControl
+              className={
+                (clsx(classes.margin, classes.textField),
+                "makeStyles-textField-16")
+              }
+              variant="outlined"
+              size="small"
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                className="input-height"
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                labelWidth={70}
+              />
+            </FormControl>
+          </div>
+          <div className="my-1">
+              <Button variant="primary" block size="sm">Sign In</Button>
           </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export class Register extends React.Component {
   constructor(props) {
