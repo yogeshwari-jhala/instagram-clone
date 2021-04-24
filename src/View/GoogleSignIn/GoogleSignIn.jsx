e2dbe040dca4e3f85f134d90f8d0264b87e203cb
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-24 22:36:11 +0530

    formatted

--- src/View/GoogleSignIn/GoogleSignIn.jsx
+++ src/View/GoogleSignIn/GoogleSignIn.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import logo from "../../assets/instalogo.png";
-import {BtnPrimary} from '../../Components/Button/Button.component'
-import {useStyles} from '../Sign/Sign.page';
-import './GoogleSignInStyle.scss';
+import { BtnPrimary } from "../../Components/Button/Button.component";
+import { useStyles } from "../Sign/Sign.page";
+import "./GoogleSignInStyle.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 
@@ -16,93 +16,93 @@ import FormControl from "@material-ui/core/FormControl";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
-
 const GoogleSignIn = (props) => {
-  
-    const classes = useStyles();
-    const [values, setValues] = React.useState({
-      password: "",
-      showPassword: false,
-      cnfpassword: '',
-    });
-    const [error, setError] = React.useState('');
-  
-    const handleChange = (prop) => (event) => {
-      setValues({ ...values, [prop]: event.target.value });
-    };
-  
-    const handleClickShowPassword = () => {
-      setValues({ ...values, showPassword: !values.showPassword });
-    };
-  
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-    return (
-      <div className="app1">
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+    cnfpassword: "",
+  });
+  const [error, setError] = React.useState("");
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="app1">
       <div className="login1">
-      <div className ="conatiner1">
-      <div className="base-container1" >
-        <div className="content1">
-          <div className="image1">
-            <img className="img1" src={logo} alt="" />
-          </div>
-          <div className="form1">
-          <FormControl
-              className={clsx(classes.margin, classes.textField)}
-              variant="outlined"
-              size="small"
-            >
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <OutlinedInput
-                id="password1"
-                type={values.showPassword ? "text" : "password"}
-                value={values.password}
-                onChange={handleChange("password")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                labelWidth={60}
-              />
-            </FormControl>
-            <FormControl
-              className={clsx(classes.margin, classes.textField)}
-              variant="outlined"
-              size="small"
-            >
-              <InputLabel htmlFor="cnfpassword">Confirm Password</InputLabel>
-              <OutlinedInput
-                id="cnfpassword1"
-                type="password"
-                value={values.cnfpassword}
-                onChange={handleChange("cnfpassword")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    
-                  </InputAdornment>
-                }
-                labelWidth={110}
-              />
-            </FormControl>
-          <BtnPrimary text="Sign In"/>
-            
+        <div className="conatiner1">
+          <div className="base-container1">
+            <div className="content1">
+              <div className="image1">
+                <img className="img1" src={logo} alt="" />
+              </div>
+              <div className="form1">
+                <FormControl
+                  className={clsx(classes.margin, classes.textField)}
+                  variant="outlined"
+                  size="small"
+                >
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <OutlinedInput
+                    id="password1"
+                    type={values.showPassword ? "text" : "password"}
+                    value={values.password}
+                    onChange={handleChange("password")}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {values.showPassword ? (
+                            <Visibility />
+                          ) : (
+                            <VisibilityOff />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    labelWidth={60}
+                  />
+                </FormControl>
+                <FormControl
+                  className={clsx(classes.margin, classes.textField)}
+                  variant="outlined"
+                  size="small"
+                >
+                  <InputLabel htmlFor="cnfpassword">
+                    Confirm Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="cnfpassword1"
+                    type="password"
+                    value={values.cnfpassword}
+                    onChange={handleChange("cnfpassword")}
+                    endAdornment={
+                      <InputAdornment position="end"></InputAdornment>
+                    }
+                    labelWidth={110}
+                  />
+                </FormControl>
+                <BtnPrimary text="Sign In" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-      </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
-
-export default GoogleSignIn
+export default GoogleSignIn;
