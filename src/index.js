f8b5033ef1b7dc30808678549118aeca43e15dec
yogeshwari27 <yogeshwarijhala1627@gmail.com>
2021-04-25 21:57:01 +0530

    post.page.jsx updated

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleSignIn from './repository/Firestore/FirebaseAuth.page'
-import {Post} from './View/Posts/Post.page'
+import Post from './View/Posts/Post.page'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GoogleSignIn />
-    {/* <Post /> */}
+    {/* <GoogleSignIn /> */}
+    <Post />
   </React.StrictMode>,
   document.getElementById('root')
 );
