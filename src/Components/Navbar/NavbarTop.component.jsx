84cfa2f90b6e06adc3f18671078860a5abf27642
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-04-28 23:00:59 +0530

    theme modified on login

--- src/Components/Navbar/NavbarTop.component.jsx
+++ src/Components/Navbar/NavbarTop.component.jsx
@@ -14,6 +14,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import Repository from '../../repository/Repository'
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -196,7 +197,9 @@ export default function NavbarTop() {
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            <IconButton aria-label="show 4 new mails" color="inherit"  className={classes.flexBtn}>
+            <IconButton aria-label="show 4 new mails" color="inherit"  className={classes.flexBtn} onClick={()=>{
+                new Repository().signOut()
+              }}>
               <Badge badgeContent={4} color="secondary">
                 <MailIcon />
               </Badge>
--- src/index.js
+++ src/index.js
@@ -3,37 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleSignIn from './repository/Firestore/FirebaseAuth.page'
 import Post from './View/Posts/Posts.page'
-import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core'
-
-let theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  }
-});
-
-theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
     <GoogleSignIn />
     {/* <Post /> */}
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
