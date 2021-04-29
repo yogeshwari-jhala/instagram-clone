c38311981164f1079ffa91b861f9147177f7fc21
yogeshwari27 <yogeshwarijhala1627@gmail.com>
2021-04-29 21:57:40 +0530

    theme set

--- src/index.js
+++ src/index.js
@@ -3,39 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GoogleSignIn from './repository/Firestore/FirebaseAuth.page'
 import Post from './View/Posts/Posts.page'
-import Profile from './View/Profile/Profile'
-import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core'
-
-let theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  }
-});
-
-theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    {/* <GoogleSignIn /> */}
-    <Post />
-    {/* <Profile/> */}
-    </ThemeProvider>
+    <GoogleSignIn />
+    {/* <Post /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
