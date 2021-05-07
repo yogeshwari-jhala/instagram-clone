c3c9cbe165b80f2aaa926fb7a1e2206f58e04af8
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-05-07 23:38:50 +0530

    modified add

--- src/repository/Firestore/Firebase.auth.js
+++ src/repository/Firestore/Firebase.auth.js
@@ -35,7 +35,6 @@ export default class FirebaseAuthProvider {
       const createdAt = Date.now();
 
       await userRef.set({
-        is_admin: false,
         displayName,
         city: null,
         country: null,
@@ -43,9 +42,6 @@ export default class FirebaseAuthProvider {
         email: email,
         username: email.split("@")[0],
         gender: null,
-        latitude: null,
-        longitute: null,
-        phoneNumber: null,
         profilePicture: photoURL,
         timestamp: createdAt,
         password: false,
